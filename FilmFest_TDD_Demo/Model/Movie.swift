da5cc61fec6722162256b8d2f82456ec2db86bee
Umer Khan <umar_s66@outlook.com>
2020-03-24 10:38:17 +0500

    Unit Testing

---
+++ FilmFest_TDD_Demo/Model/Movie.swift
@@ -0,0 +1,10 @@
+struct Movie {
+    let title: String
+    let releaseDate: String?
+    
+    init(title: String, releaseDate: String? = nil) {
+        self.title = title
+        self.releaseDate = releaseDate
+    }
+    
+}
---
+++ FilmFest_TDD_DemoTests/MovieStructsTest.swift
@@ -0,0 +1,39 @@
+//
+//  MovieStructsTest.swift
+//  FilmFest_TDD_DemoTests
+//
+//  Created by Umer Khan on 24/03/2020.
+//  Copyright © 2020 Umer Khan. All rights reserved.
+//
+
+import XCTest
+@testable import FilmFest_TDD_Demo
+
+class MovieStructsTest: XCTestCase {
+
+    override func setUp() {
+        // call before test run
+        
+    }
+
+    override func tearDown() {
+        // call after test run
+    }
+    
+    // MARK: - custom methods
+    func testInit_MovieWithTitle() {
+        let testMovie = Movie(title: "Generic Blockbuster")
+        XCTAssertNotNil(testMovie)
+        XCTAssertEqual(testMovie.title, "Generic Blockbuster")
+    }
+    
+    func testInit_TestMovieTitleAndReleaseDate() {
+        let testMovie = Movie(title: "Romantic Comedy", releaseDate: "1987")
+        
+        XCTAssertNotNil(testMovie)
+        XCTAssertEqual(testMovie.releaseDate, "1987")
+        
+        
+    }
+    
+}
